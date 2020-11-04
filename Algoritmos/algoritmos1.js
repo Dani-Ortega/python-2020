b1d67029ad75eb820b9a4678c6016b96858f7679
Loreto Ortega Guzmán <dani.g.ortega@gmail.com>
2020-11-04 18:35:17 -0300

    primercommit

---
+++ Algoritmos/algoritmos1.js
@@ -0,0 +1,20 @@
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+    0, 1, 2, 3, 4, 5
+}
+for (var i = 0; i < 5; i++) {
+    i = i + 1;
+    console.log(i);
+    2, 4, 6
+}
+for (var i = 0; i < 5; i++) {
+    i = i + 3;
+    console.log(i);
+    4, 8
+}
+
+function y(num1, num2) {
+    return num1 + num2;
+}
+console.log(y(2, 3))
+console.log(y(3, 5))
